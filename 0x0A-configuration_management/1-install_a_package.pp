91956dc428a9860d2329140b1b7b778235ea26b9
olarich11 <olarich11@gamil.com>
2023-11-24 07:11:15 -0800

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
-# Install a specific version of Flask, namely version 2.1.0.
+# Install a specific version of Flask (2.1.0.)
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pi0p3'
 }
